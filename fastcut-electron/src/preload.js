110ff60a774c0d8e2cbf7b5b6474c1650b973a2b
DigitalWorks-arch <mohamed202661@outlook.com>
2026-06-30 16:32:28 +0300

    Delete fastcut-electron directory

--- fastcut-electron/src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('fastcut', {
-  getItems: () => ipcRenderer.invoke('get-items'),
-  addItem: (title, url) => ipcRenderer.invoke('add-item', { title, url }),
-  updateShortcut: (id, shortcut) => ipcRenderer.invoke('update-shortcut', { id, shortcut }),
-  deleteItem: (id) => ipcRenderer.invoke('delete-item', { id }),
-  openItem: (url) => ipcRenderer.invoke('open-item', { url })
-});
